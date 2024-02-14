e29df79975887ef9684730386da2c765be9322b6
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-02-14 20:43:23 +0000

    Changed a link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -153,7 +153,7 @@ function Navbar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="#/"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
